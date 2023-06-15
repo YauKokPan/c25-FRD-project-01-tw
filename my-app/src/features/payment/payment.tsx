15c7cffb7f5f191fdaee9723f340befe5e7d4057
dave-hk <chunyutsui@gmail.com>
2023-06-15 17:15:10 +0800

    change payment currency

--- my-app/src/features/payment/payment.tsx
+++ my-app/src/features/payment/payment.tsx
@@ -24,6 +24,7 @@ export default function Payment() {
               purchase_units: [
                 {
                   amount: {
+                    currency_code: "HKD",
                     value: "0.01",
                   },
                 },
